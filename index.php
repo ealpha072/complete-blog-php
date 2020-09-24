389a6af0e67230df1ec7b61f55a1274a3ba191cd
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-09-24 18:09:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,18 @@
-<?php  ?>
-
-<!DOCTYPE html>
-<html>
-<head>
+<?php require_once('includes/head.php');  ?>
     <title>AlphaBlog | Home</title>
-    <link rel="stylesheet" type="text/css" href="">
-    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
-       <nav class="navbar navbar-expand-lg bg-black">
-            <a href="" class="navbar-brand"><i class="fa fa-home"></i>AlphaBlog</a>
-            <button class="navbar-toggler" data-toggle="collapse" data-target="#navlinks" type="button" aria-controls="navlinks" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="fa fa-caret-down"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navlinks">
-                <ul class="navbar-nav">
-                    <li class="nav-item"><a href="index.php" class="nav-link"><i class="fa fa-user"></i> Home</a></li>
-                    <li class="nav-item"><a href="#news" class="nav-link"><i class="fa fa-shopping-cart"></i> News</a></li>
-                    <li class="nav-item"><a href="#contact" class="nav-link"><i class="fa fa-envelope"></i>  Contact</a></li>
-                    <li class="nav-item"><a href="#about" class="nav-link"><i class="fa fa-envelope"></i>  About</a></li>
+       <?php include('includes/navbar.php'); ?>
 
-                </ul>
-            </div>
-        </nav>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>  
-</body>
-</html>
-</body>
-</html>
+       <!--banner-->
+       <?php include('includes/banner.php'); ?>
+        <!--main page content-->
+        <div class="content">
+            <h2>Recent articles</h2>
+            <hr>
+            <!--more to be added here-->  
+        </div><!--end of main page-->
+
+        <!--footer items-->
+<?php include('includes/footer.php'); ?>
