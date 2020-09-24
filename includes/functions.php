64e483fd6b07c0f6e4c27445f124cc4c37a8d104
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-09-24 22:59:39 +0300

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -5,7 +5,21 @@
         $result = mysqli_query($conn, $myquery);
         $allposts  =mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-        return $allposts;
+        $final_posts = array();
+        foreach ($final_posts as $post) {
+            $post['topic'] = getPostTopic($post['id']);
+            array_push($final_posts, $post);
+        }
+
+        return $final_posts;
+    }
+
+    function getPostTopic($post_id){
+        global $conn;
+        $query = "SELECT * FROM topic WHERE id =(SELECT topic_id FROM post_topic WHERE post_id = $post_id) LIMIT 1";
+        $result  = mysqli_query($conn, $query);
+        $topic = mysqli_fetch_assoc($result);
+        return $topic;
     }
 
 
