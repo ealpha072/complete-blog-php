c8bac93914453d6deb591b4555beba46215ca740
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-09-24 18:35:10 +0300

    Included root_path

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-<?php require_once('includes/head.php');  ?>
+<?php require_once('config.php');  ?>
+<?php require_once(ROOT_PATH . '/includes/head.php');  ?>
     <title>AlphaBlog | Home</title>
 </head>
 <body>
     <div class="container">
-       <?php include('includes/navbar.php'); ?>
+       <?php include(ROOT_PATH . '/includes/navbar.php'); ?>
 
        <!--banner-->
-       <?php include('includes/banner.php'); ?>
+       <?php include(ROOT_PATH . '/includes/banner.php'); ?>
         <!--main page content-->
         <div class="content">
             <h2>Recent articles</h2>
@@ -15,4 +16,4 @@
         </div><!--end of main page-->
 
         <!--footer items-->
-<?php include('includes/footer.php'); ?>
+<?php include(ROOT_PATH . '/includes/footer.php'); ?>
